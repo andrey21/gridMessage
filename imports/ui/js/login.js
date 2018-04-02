650bcae3ee996369825b06bd78eaf0e0ccb3f3d5
andrey21 <zareckijandrej1@gmail.com>
2018-04-02 03:15:42 +0300

    add login and account form

--- imports/ui/js/login.js
+++ imports/ui/js/login.js
@@ -1,1 +1,45 @@
 import '../html/login.html';
+
+Template.login.rendered = function(){
+
+};
+
+Template.login.events({
+    "submit .login-form": function(event){
+        var username = trimInput(event.target.username.value);
+        var password = trimInput(event.target.password.value);
+
+        if(isNotEmpty(username) && isNotEmpty(password) && isValidPassword(password)){
+            Meteor.loginWithPassword(username, password, function(err){
+                if(err){
+                    Bert.alert(err.reason, "danger", "growl-top-right");
+                    return false;
+                } else{
+                    Router.go("/sidebar");
+                    Bert.alert("You are logged in!", "success", "growl-top-right");
+                }
+            });
+        }
+        return false;
+    }
+});
+
+var trimInput = function(val){
+    return val.replace(/^\s*|\s*$/g, "");
+};
+
+var isNotEmpty = function(value){
+    if(value && value != ''){
+        return true;
+    }
+    Bert.alert('Please fill in all fields', 'danger', 'growl-top-right');
+    return false;
+}
+
+var isValidPassword = function(value){
+    if(value.length < 6){
+        Bert.alert('Password must be at least 6 characters', 'danger', 'growl-top-right');
+        return false;
+    }
+    return true;
+};
--- imports/ui/js/navbar.js
+++ imports/ui/js/navbar.js
@@ -1,2 +1,23 @@
 import '../html/navbar.html';
 
+Template.navbar.events({
+    "click .logout": function(event){
+        Meteor.logout(function(err){
+            if(err){
+                Bert.alert(err.reason, "danger", "growl-top-right");
+            } else{
+                Router.go("/");
+                Bert.alert("You are now logged out", "success", "growl-top-right");
+            }
+        });
+    }
+});
+
+Template.navbar.events({
+    "click .user_icon"(){
+        $('.ui.dropdown')
+            .dropdown()
+        ;
+    }
+});
+
